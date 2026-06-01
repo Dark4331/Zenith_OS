a9ef3cf3fc1cfc0aebcf00f281e98d48dac04894
DSK <67557009+Dark4331@users.noreply.github.com>
2026-06-01 03:46:19 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,8 +3,8 @@
 set -ouex pipefail
 
 ### Install packages
-# Installazione pulita e sicura di tutti i pacchetti di Zenith OS e dell'ambiente Niri
-dnf install -y fastfetch git curl tmux niri hyprpaper waybar fuzzel mako foot
+# Sostituito hyprpaper con swaybg (garantito nei repo ufficiali Fedora)
+dnf install -y fastfetch git curl tmux niri swaybg waybar fuzzel mako foot
 
 #### Abilita i servizi di sistema necessari
 systemctl enable podman.socket
