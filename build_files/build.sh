ec3f7c67c3f6769bed23ae43f4b1306094c69a73
DSK <67557009+Dark4331@users.noreply.github.com>
2026-06-04 01:41:19 +0200

    Add SVG logo copy to build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -49,6 +49,7 @@ mkdir -p /etc/fastfetch
 mkdir -p /usr/share/backgrounds/zenith
 cp /ctx/branding/logo.png /usr/share/plymouth/themes/spinner/watermark.png
 cp /ctx/branding/logo.png /usr/share/pixmaps/origami-logo.png
+cp /ctx/branding/logo.png /usr/share/pixmaps/origami-logo.svg
 cp /ctx/branding/wallpaper.png /usr/share/backgrounds/zenith/default.jpg
 cp /ctx/branding/ascii-logo.txt /etc/fastfetch/zenith_ascii.txt
 
