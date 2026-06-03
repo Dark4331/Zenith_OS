da3c3ea8ff3c8b909d64580b84b8cede3eacb241
DSK <67557009+Dark4331@users.noreply.github.com>
2026-06-03 19:50:44 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -47,7 +47,7 @@ cp -rf /ctx/dot_config/niri/config.kdl /etc/skel/.config/niri/
 # ---- BRANDING: SETUP ----
 mkdir -p /etc/fastfetch
 mkdir -p /usr/share/backgrounds/zenith
-cp /ctx/branding/logo.png /usr/share/plymouth/spinner/watermark.png
+cp /ctx/branding/logo.png /usr/share/plymouth/themes/spinner/watermark.png
 cp /ctx/branding/wallpaper.png /usr/share/backgrounds/zenith/default.jpg
 cp /ctx/branding/ascii-logo.txt /etc/fastfetch/zenith_ascii.txt
 
