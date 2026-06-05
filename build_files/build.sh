6c75d0761d3d436c519ac1c013db1f4dd5e7820a
DSK <67557009+Dark4331@users.noreply.github.com>
2026-06-05 22:10:55 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -62,9 +62,10 @@ cp /ctx/branding/wallpaper.png /usr/share/backgrounds/zenith/default.jpg
 cp /ctx/branding/ascii-logo.txt /etc/fastfetch/zenith_ascii.txt
 cp /ctx/branding/config.jsonc /etc/fastfetch/config.jsonc
 
+cp -f /ctx/branding/plymouthd.defaults /usr/share/plymouth/plymouthd.defaults
 mkdir -p /usr/share/plymouth/themes/hexagon/
 cp -rf /ctx/hexagon/. /usr/share/plymouth/themes/hexagon/
-
+ln -sf /usr/share/plymouth/themes/hexagon/hexagon.plymouth /usr/share/plymouth/themes/default.plymouth
 
 sed -i 's/auto-mode/manual/g' /usr/share/plymouth/themes/bgrt/bgrt.plymouth
 # Regenerate dracut for boot changes
