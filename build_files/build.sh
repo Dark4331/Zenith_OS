07f491e98f0eecfd6d9d0c53476aa7c55e0f3aa5
DSK <67557009+Dark4331@users.noreply.github.com>
2026-06-06 03:30:44 +0200

    Update GRUB configuration to disable OS prober
    Add configuration to disable OS prober in GRUB.

--- build_files/build.sh
+++ build_files/build.sh
@@ -82,7 +82,7 @@ ln -sf /usr/share/plymouth/themes/hexagon/hexagon.plymouth /usr/share/plymouth/t
 sed -i 's/auto-mode/manual/g' /usr/share/plymouth/themes/bgrt/bgrt.plymouth
 # Regenerate dracut for boot changes
 dracut --regenerate-all --force || true
-
+echo 'GRUB_DISABLE_OS_PROBER="true"' >> /etc/default/grub
 # Enable podman socket
 systemctl enable podman.socket
 
