277e79891bdbdec99ba1051d74cd0c18a1954abc
DSK <67557009+Dark4331@users.noreply.github.com>
2026-06-06 17:41:31 +0200

    Add branding files for Plymouth themes

--- build_files/build.sh
+++ build_files/build.sh
@@ -73,6 +73,7 @@ ShowDelay=0
 DeviceTimeout=8
 EOF
 #---------
+mkdir -p /usr/share/plymouth/themes/bgrt/
 cp -f /ctx/branding/bgrt.plymouth /usr/share/plymouth/themes/bgrt/bgrt.plymouth
 cp -f /ctx/branding/plymouthd.defaults /usr/share/plymouth/plymouthd.defaults
 mkdir -p /usr/share/plymouth/themes/hexagon/
